908c79600d5942ab77b6116fff0c33da972623a3
dkhomenko <dinamastercs@gmail.com>
2024-08-13 22:07:35 +0200

    add 4 tasks

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function checkForSpam(message) {
+  // Приводимо рядок до нижнього регістру
+  const normalizedMessage = message.toLowerCase();
+  
+  // Перевіряємо наявність заборонених слів
+  return normalizedMessage.includes('spam') || normalizedMessage.includes('sale');
+}
+
+// Перевірка функції
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
